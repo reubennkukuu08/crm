fee41222748086cb66df92ff0c47a8b001cb889d
reubennkukuu08 <reubennkukuu@gmail.com>
2025-02-23 18:40:54 +0300

    dynamic urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.product, name="product"),
-    path('customer/', views.customer, name="customer"),
+    path('customer/<str:pk>', views.customer, name="customer"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -24,5 +24,14 @@ def product(request):
     }
     return render(request, 'accounts/product.html', context)
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    total_orders = customer.order_set.all().count()
+
+    context = {
+        'customer' : customer,
+        'orders': orders,
+        'total_orders': total_orders
+    }
+    return render(request, 'accounts/customer.html', context)
