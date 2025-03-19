224dfb30d9f2f2fb27bd9b239d04c9fac24b00da
reubennkukuu08 <reubennkukuu@gmail.com>
2025-03-19 11:34:26 +0300

    Added build.sh bash file

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# build.sh
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
