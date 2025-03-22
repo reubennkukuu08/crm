8fe13bddb3b169746c6cea3cf32676898184052b
reubennkukuu08 <reubennkukuu@gmail.com>
2025-03-22 22:09:48 +0300

    updated settings.py to handle dev and deployment requests correctly

--- crm/settings.py
+++ crm/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import dj_database_url
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "FALSE").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -88,8 +91,10 @@ DATABASES = {
 }
 
 
-database_url = os.environ.get("DATABASE_URL")
-DATABASES['default'] = dj_database_url.parse(database_url)
+if not DEBUG:
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url:
+        DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
